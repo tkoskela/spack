b8cddd2402ccfa321e8c4eb9905cfcba88a1c523
Tuomas Koskela <t.koskela@ucl.ac.uk>
2024-10-04 17:03:24 +0100

    Add executables to path, clean up dependencies

--- var/spack/repos/builtin/packages/sopt/package.py
+++ var/spack/repos/builtin/packages/sopt/package.py
@@ -6,9 +6,9 @@
 from spack.package import *
 
 class Sopt(CMakePackage):
-    """SOPT is an open-source C++ package available under the license below. It performs 
-    Sparse OPTimisation using state-of-the-art convex optimisation algorithms. It solves a 
-    variety of sparse regularisation problems, including the Sparsity Averaging Reweighted 
+    """SOPT is an open-source C++ package available under the license below. It performs
+    Sparse OPTimisation using state-of-the-art convex optimisation algorithms. It solves a
+    variety of sparse regularisation problems, including the Sparsity Averaging Reweighted
     Analysis (SARA) algorithm.
     """
 
@@ -18,7 +18,7 @@ class Sopt(CMakePackage):
 
     maintainers("tkoskela", "mmcleod89", "20DM")
     license("GPL-2.0")
-    
+
     version("4.2.0", sha256="25e579722f8e049d37c9155affa57ec2f38a2f8414c9cf430da2b7bafc86907b")
 
     variant("tests", default=True, description="Build tests")
@@ -31,7 +31,7 @@ class Sopt(CMakePackage):
     variant("cppflow", default=False, description="Build with Tensorflow support using cppflow")
     variant("onnxrt", default=False, description="Build with Tensorflow support using onnx")
 
-    depends_on("cmake@3.30")
+    depends_on("cmake@3")
     depends_on("eigen@3.4")
     depends_on("libtiff@4.5")
     depends_on("catch2@3.4", when="+tests")
@@ -50,9 +50,17 @@ class Sopt(CMakePackage):
             self.define_from_variant("dompi", "mpi"),
             self.define_from_variant("onnxrt", "onnxrt"),
             self.define_from_variant("coverage", "coverage"),
-            self.define_from_variant("cppflow", "cppflow")
         ]
         return args
+
+
+    def setup_run_environment(self, env):
+        if "+tests" in self.spec:
+            env.prepend_path("PATH", self.spec.prefix.tests)
+        if "+examples" in self.spec:
+            env.prepend_path("PATH", join_path(self.spec.prefix, "examples"))
+        if "+benchmarks" in self.spec:
+            env.prepend_path("PATH", join_path(self.spec.prefix, "benchmarks"))
 
     def install(self, spec, prefix):
         with working_dir(self.build_directory):
@@ -63,4 +71,3 @@ class Sopt(CMakePackage):
                 install_tree("cpp/examples", join_path(spec.prefix, "examples"))
             if "+benchmarks" in spec:
                 install_tree("cpp/benchmarks", join_path(spec.prefix, "benchmarks"))
-
